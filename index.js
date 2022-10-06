7177980c9303ec534e7c00dd62247fdeec86f6f3
seemrcola <seemrcola@163.com>
2022-10-06 15:42:02 +0800

    feat: add repo

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const { program } = require('commander')
 const commanderHelp = require('./lib/core/help')
-const createCommand = require('./lib/core/create')
+const { createCommand, addCpnCommand, addRepoCommand } = require('./lib/core/cli')
 const pkg = require('./package.json')
 
 /* get program help */
 commanderHelp(program)
+
 /* generator command */
 createCommand(program)
+addCpnCommand(program)
+addRepoCommand(program)
 
 /* add version */
 program.version(pkg.version)
