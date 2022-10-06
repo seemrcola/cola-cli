46ebfa830331494a34af12162bb5db4cd8e292d3
seemrcola <seemrcola@163.com>
2022-10-06 23:27:48 +0800

    feat: repo list & setrepo & delrepo

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { program } = require('commander')
-const commanderHelp = require('./lib/core/help')
-const { createCommand, addCpnCommand, addRepoCommand } = require('./lib/core/cli')
 const pkg = require('./package.json')
+const commanderHelp = require('./lib/core/help')
+const {
+  createCommand,
+  addCpnCommand,
+  addRepoCommand,
+  setRepoCommand,
+  deleteRepoCommand,
+  getRepoListCommand
+} = require('./lib/core/cli')
 
 /* get program help */
 commanderHelp(program)
@@ -10,6 +17,9 @@ commanderHelp(program)
 createCommand(program)
 addCpnCommand(program)
 addRepoCommand(program)
+setRepoCommand(program)
+getRepoListCommand(program)
+deleteRepoCommand(program)
 
 /* add version */
 program.version(pkg.version)
